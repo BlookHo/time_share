16c91ba63d9517f7e147257ecba5a9a3982dd3a3
Aleksey <zoneiva@gmail.com>
2018-03-26 12:44:06 +0300

    Initial commit for the TimeShare project
    Content of initial actions:
     - RoR API project generated
     - database.yml + secrets.yml created with dev., test and prod. databases data
     - PG connected
     - Resort and Apartment models generated and migrated
     - resort has many apartments
    
    No connection to any #issue

---
+++ app/graphql/time_share_schema.rb
@@ -0,0 +1,4 @@
+TimeShareSchema = GraphQL::Schema.define do
+  mutation(Types::MutationType)
+  query(Types::QueryType)
+end
---
+++ db/migrate/20180326082319_create_apartments.rb
@@ -0,0 +1,11 @@
+class CreateApartments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :apartments do |t|
+      t.string :type
+      t.string :description
+      t.references :resort, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
